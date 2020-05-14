6657fe834146210b883eaf5facf4d280bdebb437
xuyg <xuyuangeng25@hotmail.com>
2020-05-15 00:57:45 +0800

    api-gateway init

---
+++ src/test/java/com/imooc/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
